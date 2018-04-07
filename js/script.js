42886f5d92bf4c5d0bf600d97aa7de31ed92f502
mkurzyk <37752301+mkurzyk@users.noreply.github.com>
2018-04-07 21:22:07 +0200

    updated

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
-function drawTree(x) 
-
-{
-
-	for (i=1; i<=x; i++) {
-	 	for (j = i; j < x; j++)
-        	{
-            		document.write(" ");
-        	}
-		for (y=1; y <= i; y++) 
-		{
-			document.write('*');
+function drawTree(x) {
+	for (i=0; i < x; i++) {
+		var star = '';
+		for (j=0; j <=i; j++) {
+			star += '*';
 		}
-			document.write('<br/>');
-		}
+		console.log(star);
+	}
 }
 
 drawTree(prompt("Podaj liczbę gałęzi"));
