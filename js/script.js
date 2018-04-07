5f351ccce912820a0b03e656d951e02717da7a5c
mkurzyk <37752301+mkurzyk@users.noreply.github.com>
2018-04-07 19:22:58 +0200

    update 2

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ function drawTree(x)
 {
 
 	for (i=1; i<=x; i++) {
-	 	for (j = i; j < n; j++)
+	 	for (j = i; j < x; j++)
         	{
-            		document.write("");
+            		document.write(" ");
         	}
 		for (y=1; y <= i; y++) 
 		{
